11a5f363049c3058e7fecd51ee2b2bf6d4aa555c
Suyash Mittal <suyash8514@gmail.com>
2018-05-20 12:17:02 +0530

    added UVa problem no 108

---
+++ uva/108.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSum=0;
+
+int kadane(int n, int *arr){
+    int max = 0;
+    int maxSoFar = 0;
+    for(int i=0; i < n; i++){
+        maxSoFar += arr[i];
+        if(maxSoFar < 0){
+            maxSoFar = 0;
+        }
+        if(max < maxSoFar){
+            max = maxSoFar;
+        }
+    }
+    return max;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n][n];
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin >> arr[i][j];
+    int temp[n];
+    for(int left = 0; left < n ; left++){
+        for(int i=0; i < n; i++){
+            temp[i] = 0;
+        }
+        for(int right = left; right < n; right++){
+            for(int i=0; i < n; i++){
+                temp[i] += arr[i][right];
+            }
+            int res = kadane(n, temp);
+            if(res > maxSum){
+                maxSum = res;
+            }
+        }
+    }
+    cout << maxSum << endl;
+    return 0;
+}
